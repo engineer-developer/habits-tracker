b17cda5e7615509c377effafdd422d073e76e69b
Alex Suslov <suslov.lexx@gmail.com>
2025-09-30 21:38:42 +0500

    upd src.fastapi_app.config.Settings

--- fastapi_app/src/fastapi_app/config.py
+++ fastapi_app/src/fastapi_app/config.py
@@ -7,8 +7,4 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     """Настройки сервиса."""
 
-    db_url: PostgresDsn = Field(
-        validation_alias="DB_URL",
-        default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5004/db",
-    )
     db_url: PostgresDsn = Field(validation_alias="DB_URL")
