61200d65c2891e37aee685c5ed760e95c17f8d17
Alex Suslov <suslov.lexx@gmail.com>
2025-09-30 21:38:04 +0500

    upd fastapi_app/src/fastapi_app/config.py:10

--- fastapi_app/src/fastapi_app/config.py
+++ fastapi_app/src/fastapi_app/config.py
@@ -11,3 +11,4 @@ class Settings(BaseSettings):
         validation_alias="DB_URL",
         default="postgresql+asyncpg://postgres:postgres@0.0.0.0:5004/db",
     )
+    db_url: PostgresDsn = Field(validation_alias="DB_URL")
