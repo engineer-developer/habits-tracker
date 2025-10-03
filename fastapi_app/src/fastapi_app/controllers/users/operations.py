04b8cbfa8a337fe935553880bcb2f63dcd224b53
Alex Suslov <suslov.lexx@gmail.com>
2025-10-03 22:03:19 +0500

    add controllers package

---
+++ fastapi_app/src/fastapi_app/controllers/users/operations.py
@@ -0,0 +1,21 @@
+from dao.models import User
+from sqlalchemy import select
+from sqlalchemy.exc import DatabaseError
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def add_user(session: AsyncSession, user: User):
+    session.add(user)
+    try:
+        await session.commit()
+        return user
+    except DatabaseError as exc:
+        return exc
+
+
+
+async def fetch_all_users(session: AsyncSession):
+    stmt = select(User).where(User.is_active==True)
+    result = await session.execute(stmt)
+    users = result.scalars().all()
+    return users
