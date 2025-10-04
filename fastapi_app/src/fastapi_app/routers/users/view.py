038a2217c8118d89ecb2f15bb35b82578305a934
Alex Suslov <suslov.lexx@gmail.com>
2025-10-04 18:24:33 +0500

    add routers.users.view.add_user

--- fastapi_app/src/fastapi_app/routers/users/view.py
+++ fastapi_app/src/fastapi_app/routers/users/view.py
@@ -1,11 +1,12 @@
-from fastapi import HTTPException
-from fastapi.responses import JSONResponse
 """Модуль представлений пользователей."""
 
 from controllers.users.operations import add_user_to_db, fetch_all_users
+from dao.models import User
 from database.manager import CommonAsyncSession
 from dto.schemas import UserInSchema, UserOutSchema, UsersListSchema
+from fastapi.exceptions import HTTPException
 from fastapi.routing import APIRouter
+from sqlalchemy.exc import IntegrityError
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -14,8 +15,6 @@ router = APIRouter(prefix="/users", tags=["users"])
     "",
     name="Get all users",
     description="Get all users from database",
-    response_class=JSONResponse,
-    responses={400: {"description": "No content"}},
     response_model=UsersListSchema,
     responses={404: {"description": "Users not found"}},
 )
@@ -26,16 +25,30 @@ async def get_all_users(session: CommonAsyncSession) -> UsersListSchema:
     return UsersListSchema(users=users_sch)
 
 
+@router.post(
+    "",
+    name="Add user",
+    description="Add user to database",
+    response_model=UserOutSchema,
+    status_code=201,
+    responses={
+        403: {
+            "description": "Ошибка уникальности пользователя",
+            "content": {"application/json": {"example": {"detail": "string"}}},
+        }
+    },
 )
-async def get_all_users():
-    # users_dao = fetch_users_from_db()
-    users = [
-        {"name": "Bob"},
-        {"name": "Jack"},
-        {"name": "Piter"},
-    ]
-    content = {"users": users}
-    if content:
-        return JSONResponse(content)
+async def add_user(user: UserInSchema, session: CommonAsyncSession) -> UserOutSchema:
+    """Точка добавления пользователя."""
+    user_orm = User(**user.model_dump())
+    user_from_db = await add_user_to_db(user=user_orm, session=session)
+
+    if user_from_db and isinstance(user_from_db, User):
+        return UserOutSchema.model_validate(user_from_db)
+    elif user_from_db and isinstance(user_from_db, IntegrityError):
+        raise HTTPException(
+            status_code=403,
+            detail=f"Пользователь с telegram_id {user.telegram_id} уже есть.",
+        )
     else:
-        raise HTTPException(400, "No content")
+        raise HTTPException(status_code=400, detail="Bad request")
