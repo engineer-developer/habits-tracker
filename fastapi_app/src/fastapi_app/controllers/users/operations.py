c59f2760560a608456961fb9cceafc575e3499a6
Alex Suslov <suslov.lexx@gmail.com>
2025-10-04 18:19:13 +0500

    add controllers.users.operations.fetch_all_users

--- fastapi_app/src/fastapi_app/controllers/users/operations.py
+++ fastapi_app/src/fastapi_app/controllers/users/operations.py
@@ -1,3 +1,6 @@
+from typing import Sequence
+
+from app_logging.logger import logger
 from dao.models import User
 from sqlalchemy import select
 from sqlalchemy.exc import DatabaseError
@@ -5,6 +8,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def add_user(session: AsyncSession, user: User):
+async def fetch_all_users(session: AsyncSession) -> Sequence[User]:
+    """Извлекаем всех активных пользователей из БД"""
+    stmt = select(User).where(User.is_active == True)
+    result = await session.execute(stmt)
+    users = result.scalars().all()
+    logger.debug("Got users: {}", users)
+    return users
+
+
     session.add(user)
     try:
         await session.commit()
