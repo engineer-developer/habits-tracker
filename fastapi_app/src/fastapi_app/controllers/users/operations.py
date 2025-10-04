4298b64e79d1b1586bfb427dbfd02370b21037e2
Alex Suslov <suslov.lexx@gmail.com>
2025-10-04 18:19:45 +0500

    add controllers.users.operations.add_user_to_db

--- fastapi_app/src/fastapi_app/controllers/users/operations.py
+++ fastapi_app/src/fastapi_app/controllers/users/operations.py
@@ -1,13 +1,13 @@
 from typing import Sequence
 
 from app_logging.logger import logger
+from controllers.auth.password_handler import get_password_hash
 from dao.models import User
 from sqlalchemy import select
 from sqlalchemy.exc import DatabaseError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def add_user(session: AsyncSession, user: User):
 async def fetch_all_users(session: AsyncSession) -> Sequence[User]:
     """Извлекаем всех активных пользователей из БД"""
     stmt = select(User).where(User.is_active == True)
@@ -17,17 +17,15 @@ async def fetch_all_users(session: AsyncSession) -> Sequence[User]:
     return users
 
 
+async def add_user_to_db(user: User, session: AsyncSession):
+    """Добавляем пользователя в базу данных"""
+    user_hash_password = await get_password_hash(user.password)
+    user.password = user_hash_password
     session.add(user)
     try:
         await session.commit()
+        logger.info("User {} added.", user.name)
         return user
     except DatabaseError as exc:
+        logger.error("{} - {}", exc.__class__.__name__, exc.args[0])
         return exc
-
-
-
-async def fetch_all_users(session: AsyncSession):
-    stmt = select(User).where(User.is_active==True)
-    result = await session.execute(stmt)
-    users = result.scalars().all()
-    return users
