cabfbbe20e4441ce587efe897f173620612e69c0
Alex Suslov <suslov.lexx@gmail.com>
2025-10-11 21:04:11 +0500

    upd bot_factory.py

--- tg_bot/src/tg_bot/core/bot_factory.py
+++ tg_bot/src/tg_bot/core/bot_factory.py
@@ -1,7 +1,11 @@
+"""Модуль инициализации бота."""
+
 from telebot import TeleBot
 from telebot.types import BotCommand
-from .config import settings
 
+from core.config import get_settings
 
-bot = TeleBot(token=settings.bot_token, parse_mode=None)
+settings = get_settings()
+
+bot = TeleBot(token=settings.bot_token)
 bot.set_my_commands([BotCommand("start", "Начнем работу")])
