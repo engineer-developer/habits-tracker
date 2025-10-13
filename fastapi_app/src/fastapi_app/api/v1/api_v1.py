fcb5278ddde4906513348d27c51add02019b03f1
Alex Suslov <suslov.lexx@gmail.com>
2025-10-13 19:42:39 +0500

    upd api.v1.api_v1.get_api_version

--- fastapi_app/src/fastapi_app/api/v1/api_v1.py
+++ fastapi_app/src/fastapi_app/api/v1/api_v1.py
@@ -9,7 +9,12 @@ router = APIRouter(prefix="/api/v1", )
 router.include_router(users_router)
 
 
-@router.get("/version/", name="Get api version", description="Get current api version", tags=["API"])
+@router.get(
+    "/version/",
+    name="Get api version",
+    description="Get current api version",
+    tags=["API"],
+)
 async def get_api_version():
     logger.debug("Api version: {}", API_VERSION)
     return {"api_version": API_VERSION}
