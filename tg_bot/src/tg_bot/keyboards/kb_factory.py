96f6ffcda5bb60532acbb427fef6cc91b13f3c79
Alex Suslov <suslov.lexx@gmail.com>
2025-10-13 20:00:37 +0500

    upd kb_factory.py

--- tg_bot/src/tg_bot/keyboards/kb_factory.py
+++ tg_bot/src/tg_bot/keyboards/kb_factory.py
@@ -1,14 +1,16 @@
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+"""Модуль генерации keyboards."""
+
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def get_start_kb(text: str, callback: str) -> InlineKeyboardMarkup:
+def get_start_kb(text: str, callback_data: str) -> InlineKeyboardMarkup:
     """Получаем стартовую клавиатуру."""
     markup = InlineKeyboardMarkup(row_width=1)
-    button = InlineKeyboardButton(text=text, callback=callback)
+    button = InlineKeyboardButton(text=text, callback_data=callback_data)
     markup.add(button)
     return markup
 
 
-main_menu_kb = get_start_kb("Главное меню", "cb_main_menu")
+personal_account_kb = get_start_kb("Личный кабинет", "cb_personal_account")
 login_kb = get_start_kb("Войти", "cb_login")
 register_kb = get_start_kb("Зарегистрироваться", "cb_register")
