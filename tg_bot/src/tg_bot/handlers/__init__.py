85871fe9d74962f7168f7233423f744f36377b6a
Alex Suslov <suslov.lexx@gmail.com>
2025-10-14 00:11:35 +0500

    add message_handlers.py

--- tg_bot/src/tg_bot/handlers/__init__.py
+++ tg_bot/src/tg_bot/handlers/__init__.py
@@ -5,9 +5,11 @@ from telebot import TeleBot
 
 from handlers.callback_handlers import register_callback_handlers
 from handlers.command_handlers import register_command_handlers
+from handlers.message_handlers import register_message_handlers
 
 
 def register_handlers(bot: TeleBot, settings: Settings) -> None:
     """Функция регистрации обработчиков."""
     register_command_handlers(bot, settings)
     register_callback_handlers(bot, settings)
+    register_message_handlers(bot, settings)
--- tg_bot/src/tg_bot/handlers/message_handler.py
+++
@@ -1,6 +0,0 @@
-from telebot import TeleBot
-from telebot.types import Message
-
-
-def echo_all(message: Message, bot: TeleBot):
-    bot.reply_to(message, message.text)
