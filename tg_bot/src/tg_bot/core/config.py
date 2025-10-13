78a0376112ef0578b4428c8eb70467af90259af7
Alex Suslov <suslov.lexx@gmail.com>
2025-10-13 20:04:13 +0500

    upd config.py: add .env path

--- tg_bot/src/tg_bot/core/config.py
+++ tg_bot/src/tg_bot/core/config.py
@@ -5,18 +5,19 @@ from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-BASE_DIR = Path(__file__).parent.parent
+BASE_DIR = Path(__file__).parent.parent.parent.parent
 
 
 class Settings(BaseSettings):
     """Настройки приложения."""
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=BASE_DIR / ".env")
 
     bot_token: str = Field(validation_alias="BOT_TOKEN")
     api_url: str = Field(validation_alias="FASTAPI_API_URL")
 
 
-def get_settings():
+def get_settings() -> Settings:
     """Получение экземпляра класса настроек."""
-    return Settings()
+    settings = Settings()
+    return settings
