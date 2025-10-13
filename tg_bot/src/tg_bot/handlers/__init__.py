d1f931f58e776896989241224d1d646f32321a08
Alex Suslov <suslov.lexx@gmail.com>
2025-10-13 20:38:11 +0500

    upd __init__.py: add register_handlers

---
+++ tg_bot/src/tg_bot/handlers/__init__.py
@@ -0,0 +1,13 @@
+"""Модуль регистрации обработчиков commands, callbacks, messages."""
+
+from core.config import Settings
+from telebot import TeleBot
+
+from handlers.callback_handlers import register_callback_handlers
+from handlers.command_handlers import register_command_handlers
+
+
+def register_handlers(bot: TeleBot, settings: Settings) -> None:
+    """Функция регистрации обработчиков."""
+    register_command_handlers(bot, settings)
+    register_callback_handlers(bot, settings)
