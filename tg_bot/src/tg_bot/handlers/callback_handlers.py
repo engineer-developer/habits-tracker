8ed76af1713d4204605487402edeb71e6741f880
Alex Suslov <suslov.lexx@gmail.com>
2025-10-14 00:11:06 +0500

    upd callback_handlers.py

--- tg_bot/src/tg_bot/handlers/callback_handlers.py
+++ tg_bot/src/tg_bot/handlers/callback_handlers.py
@@ -1,21 +1,21 @@
-from requests import Response, codes
+"""Модуль обработки callback."""
+
+from core.config import Settings
+from states.states import AuthStates
 from telebot import TeleBot
 from telebot.types import CallbackQuery
 
-from api_requests.funcs import get_request_session_with_headers
-from core.config import Settings
 
-
-def register_callback_handlers(bot: TeleBot, settings: Settings):
+def register_callback_handlers(bot: TeleBot, settings: Settings) -> None:
+    """Функция регистрации обработчиков callback."""
 
     @bot.callback_query_handler(func=lambda callback: callback.data == "cb_register")
-    def make_request_for_register(callback: CallbackQuery):
-        request_session = get_request_session_with_headers(
-            telegram_id=callback.from_user.id
+    def query_password_for_register(callback: CallbackQuery) -> None:
+        """Функция обработки callback cb_register."""
+        user_id = callback.from_user.id
+        chat_id = callback.message.chat.id
+        bot.set_state(user_id=user_id, chat_id=chat_id, state=AuthStates.wait_password)
+        bot.send_message(
+            chat_id=callback.message.chat.id, text="Отправьте пароль для регистрации."
         )
-        url = settings.api_url + "register/"
-        response: Response = request_session.get(url=url)
-
-        if response.status_code == codes.OK:
-            bot.answer_callback_query(callback_query_id=callback.id, text="Успешно зарегистрирован.")
-
+        bot.answer_callback_query(callback_query_id=callback.id)
