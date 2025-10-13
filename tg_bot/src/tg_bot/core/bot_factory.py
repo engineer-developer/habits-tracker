06eebcece5b79b06b4980646b4753477fe03c76e
Alex Suslov <suslov.lexx@gmail.com>
2025-10-13 20:01:37 +0500

    upd bot_factory.py

--- tg_bot/src/tg_bot/core/bot_factory.py
+++ tg_bot/src/tg_bot/core/bot_factory.py
@@ -1,11 +1,13 @@
 """Модуль инициализации бота."""
 
-from telebot import TeleBot
+from telebot import StateMemoryStorage, TeleBot
 from telebot.types import BotCommand
 
 from core.config import get_settings
 
 settings = get_settings()
+storage = StateMemoryStorage()
 
-bot = TeleBot(token=settings.bot_token)
+
+bot = TeleBot(token=settings.bot_token, state_storage=storage)
 bot.set_my_commands([BotCommand("start", "Начнем работу")])
