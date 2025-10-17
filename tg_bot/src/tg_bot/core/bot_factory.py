2f5236708c9767af4c8586414a3f3f8030cd8e28
Alex Suslov <suslov.lexx@gmail.com>
2025-10-17 21:19:51 +0500

    add custom filter !!!

--- tg_bot/src/tg_bot/core/bot_factory.py
+++ tg_bot/src/tg_bot/core/bot_factory.py
@@ -1,7 +1,7 @@
 """Модуль инициализации бота."""
 
 from states.states import state_storage
-from telebot import TeleBot
+from telebot import TeleBot, custom_filters
 from telebot.types import BotCommand
 
 from core.config import get_settings
@@ -10,3 +10,4 @@ settings = get_settings()
 
 bot = TeleBot(token=settings.bot_token, state_storage=state_storage)
 bot.set_my_commands([BotCommand("start", "Начнем работу")])
+bot.add_custom_filter(custom_filter=custom_filters.StateFilter(bot))
