f2633e42ab03f21716fe9be5e6991c825501b4af
Alex Suslov <suslov.lexx@gmail.com>
2025-10-23 16:25:04 +0500

    upd api.auth.dependencies.get_current_user

--- fastapi_app/src/fastapi_app/api/auth/dependencies.py
+++ fastapi_app/src/fastapi_app/api/auth/dependencies.py
@@ -12,24 +12,17 @@ from services.user_service import fetch_user_by_telegram_id
 
 
 async def get_current_user(
-    token_data: Annotated[dict, Depends(verify_access_token)],
+    telegram_id: Annotated[str, Depends(verify_access_token)],
     session: CommonAsyncSession,
 ) -> Optional[User]:
     """Получаем текущего пользователя.
 
-    :param token_data: Данные из токена аутентификации, получаемые через зависимость.
+    :param telegram_id: Данные из токена аутентификации, получаемые через зависимость.
     :param session: Асинхронная сессия взаимодействия с БД.
     :return: User - пользователь на основе данных из токена.
     :rtype: User.
-    :raise 400: Не найден telegram_id.
     :raise 404: Пользователь не найден.
     """
-    telegram_id = token_data.get("telegram_id")
-
-    if telegram_id is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Не найден telegram_id."
-        )
     if isinstance(telegram_id, str) and telegram_id.isdigit():
         telegram_id = int(telegram_id)
 
