abac6bfc98beb905b75354d2a169d2e078c06735
Alex Suslov <suslov.lexx@gmail.com>
2025-10-24 23:20:15 +0500

    make migrations: add table habits

--- fastapi_app/src/fastapi_app/migrations/versions/2025_10_24_2318-6c68dba6787c_add_table_habits.py
+++ fastapi_app/src/fastapi_app/migrations/versions/2025_10_24_2318-6c68dba6787c_add_table_habits.py
@@ -1,8 +1,8 @@
 """add table habits
 
-Revision ID: 40375ca12763
+Revision ID: 6c68dba6787c
 Revises: 394f1a8cdbf1
-Create Date: 2025-10-24 03:45:32.573390
+Create Date: 2025-10-24 23:18:43.560366
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '40375ca12763'
+revision: str = '6c68dba6787c'
 down_revision: Union[str, Sequence[str], None] = '394f1a8cdbf1'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -23,11 +23,12 @@ def upgrade() -> None:
     op.create_table('habits',
     sa.Column('name', sa.String(length=100), nullable=False),
     sa.Column('description', sa.String(), server_default='', nullable=False),
+    sa.Column('user_id', sa.Integer(), nullable=False),
     sa.Column('created_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
     sa.Column('updated_at', sa.DateTime(), server_default=sa.text('now()'), nullable=False),
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
-    sa.PrimaryKeyConstraint('id', name=op.f('pk_habits')),
-    sa.UniqueConstraint('name', name=op.f('uq_habits_name_'))
+    sa.ForeignKeyConstraint(['user_id'], ['users.id'], name=op.f('fk_habits_user_id_users'), ondelete='CASCADE'),
+    sa.PrimaryKeyConstraint('id', name=op.f('pk_habits'))
     )
 
 
