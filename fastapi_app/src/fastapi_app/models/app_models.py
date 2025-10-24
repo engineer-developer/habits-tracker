ce48254b8cfd3e3499e35db6c3ceadce17272968
Alex Suslov <suslov.lexx@gmail.com>
2025-10-24 23:34:11 +0500

    upd models.app_models.User with habits rel

--- fastapi_app/src/fastapi_app/models/app_models.py
+++ fastapi_app/src/fastapi_app/models/app_models.py
@@ -19,10 +19,7 @@ class User(TimestampMixin, Model):
         default=True,
         server_default=text("true"),
     )
-    habits: Mapped[list["Habit"]] = relationship(
-        secondary="association_table",
-        back_populates="users",
-    )
+    habits: Mapped[list["Habit"]] = relationship(back_populates="user")
 
     def __repr__(self):
         return f"<User id-{self.id}>"
