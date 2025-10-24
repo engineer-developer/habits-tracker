896838e946d404686e25b3c46f538fcb89335c9f
Alex Suslov <suslov.lexx@gmail.com>
2025-10-24 23:05:01 +0500

    add api.auth.token_handler.get_access_token

--- fastapi_app/src/fastapi_app/api/auth/token_handler.py
+++ fastapi_app/src/fastapi_app/api/auth/token_handler.py
@@ -19,6 +19,14 @@ EXPIRE_TIME = settings.access_token_expire_minutes
 auth_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login/")
 
 
+def get_access_token(token: Annotated[str | None, Depends(auth_scheme)] = None) -> str:
+    """Получаем токен аутентификации."""
+    if not token:
+        raise HTTPException(404, "Токен аутентификации не найден.")
+    logger.debug("Получен токен: {}", token)
+    return token
+
+
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     """Создаем jwt-токен."""
     to_encode = data.copy()
