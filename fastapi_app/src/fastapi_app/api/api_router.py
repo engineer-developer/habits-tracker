7a3f7fc1363b317a8f1afea3726d4f402feba1db
Alex Suslov <suslov.lexx@gmail.com>
2025-11-02 21:42:15 +0500

    add habits_router to api_router

--- fastapi_app/src/fastapi_app/api/api_router.py
+++ fastapi_app/src/fastapi_app/api/api_router.py
@@ -4,14 +4,17 @@ from core.loguru_config import logger
 from fastapi.routing import APIRouter
 
 from api.auth.auth_router import router as auth_router
+from api.habits.habits_router import router as habits_router
 from api.users.user_router import router as users_router
 
 API_VERSION = "1.0.0"
+
 
 router = APIRouter(prefix="/api")
 
 router.include_router(auth_router)
 router.include_router(users_router)
+router.include_router(habits_router)
 
 
 @router.get(
