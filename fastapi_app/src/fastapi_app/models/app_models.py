ba38fe01a1dd3b0636594810dc04de19780a4aa4
Alex Suslov <suslov.lexx@gmail.com>
2025-11-02 21:20:46 +0500

    upd models/app_models.py

--- fastapi_app/src/fastapi_app/models/app_models.py
+++ fastapi_app/src/fastapi_app/models/app_models.py
@@ -1,7 +1,19 @@
-from sqlalchemy import BigInteger, Boolean, text, String, ForeignKey
+"""Модуль определения моделей таблиц базы данных."""
+
+from datetime import datetime, time
+
+from sqlalchemy import (
+    BigInteger,
+    ForeignKey,
+    String,
+    UniqueConstraint,
+    text,
+    CheckConstraint,
+)
+from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from models.base_model import TimestampMixin, Model, Base
+from models.base_model import Model, TimestampMixin
 
 
 class User(TimestampMixin, Model):
@@ -14,51 +26,46 @@ class User(TimestampMixin, Model):
     last_name: Mapped[str] = mapped_column(nullable=True)
     username: Mapped[str] = mapped_column(nullable=True)
     password: Mapped[str]
-    is_active: Mapped[bool] = mapped_column(
-        Boolean,
-        default=True,
-        server_default=text("true"),
-    )
-    habits: Mapped[list["Habit"]] = relationship(back_populates="user")
+    is_active: Mapped[bool] = mapped_column(default=True, server_default=text("true"))
 
-    def __repr__(self):
-        return f"<User id-{self.id}>"
+    habits: Mapped[list["Habit"]] = relationship(back_populates="user")
 
 
 class Habit(TimestampMixin, Model):
     """Модель привычки."""
 
     __tablename__ = "habits"
+    __table_args__ = (UniqueConstraint("name", "user_id"),)
 
     name: Mapped[str] = mapped_column(String(100))
     description: Mapped[str] = mapped_column(default="", server_default="")
+    is_active: Mapped[bool] = mapped_column(default=True, server_default=text("true"))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
 
-    user: Mapped[list["User"]] = relationship(back_populates="habits")
+    user: Mapped["User"] = relationship(back_populates="habits")
+    reminder: Mapped["Reminder"] = relationship(back_populates="habit")
+    tracking: Mapped[list["Tracking"]] = relationship(back_populates="habit")
 
-    def __repr__(self):
-        return f"<Habit id-{self.id}>"
+
+class Reminder(TimestampMixin, Model):
+    """Модель напоминания."""
+
+    __tablename__ = "reminders"
+
+    remind_time: Mapped[time]
+    remind_quantity: Mapped[int]
+    job_id: Mapped[str] = mapped_column(unique=True)
+    habit_id: Mapped[int] = mapped_column(ForeignKey("habits.id", ondelete="CASCADE"))
+
+    habit: Mapped["Habit"] = relationship(back_populates="reminder")
 
 
-class Tracking(Model):
-    """Модель для отслеживания привычек."""
+class Tracking(TimestampMixin, Model):
+    """Модель для отслеживания выполненных привычек."""
 
-    __tablename__ = "trackings"
-    __table_args__ = (
-        CheckConstraint("remind_count >= 0", name="cnt_positive_remind_count"),
-    )
+    __tablename__ = "tracking"
 
-    remind_count: Mapped[int] = mapped_column(
-        default=21, comment="Количество напоминаний"
-    )
-    remind_time: Mapped[time] = mapped_column(comment="Время напоминания")
-    chat_id: Mapped[int] = mapped_column(comment="ID чата")
-    job_id: Mapped[str] = mapped_column(comment="ID запланированной задачи")
+    alert_time: Mapped[datetime]=mapped_column(TIMESTAMP(timezone=True))
+    habit_id: Mapped[int] = mapped_column(ForeignKey("habits.id", ondelete="CASCADE"))
 
-    habit_id: Mapped[int] = mapped_column(
-        ForeignKey("habits.id", ondelete="CASCADE"), comment="ID привычки"
-    )
-    habit: Mapped["Habit"] = relationship(back_populates="trackings")
-
-    def __repr__(self):
-        return f"<Tracking id-{self.id}>"
+    habit: Mapped["Habit"] = relationship(back_populates="tracking")
