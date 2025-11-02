b149340e8d431d434a2ef59dfc69635dd9f5619b
Alex Suslov <suslov.lexx@gmail.com>
2025-11-02 21:42:56 +0500

    upd api/users/user_router.py

--- fastapi_app/src/fastapi_app/api/users/user_router.py
+++ fastapi_app/src/fastapi_app/api/users/user_router.py
@@ -1,27 +1,24 @@
 """Модуль представлений пользователей."""
 
-from typing import Annotated, Sequence
+from typing import Annotated
 
-from core.database import CommonAsyncSession
-from fastapi.exceptions import HTTPException
-from fastapi.params import Depends, Query
+from fastapi.params import Depends
 from fastapi.routing import APIRouter
 from models.app_models import User
-from schemas.user_schema import UserInSchema, UserOutSchema, UsersListSchema
-from services.user_service import add_user_to_db, fetch_all_users
+from schemas.user_schema import UserOutDto
 
 from api.auth.dependencies import get_current_active_user
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.get("/profile/", response_model=UserOutSchema, status_code=200)
+@router.get("/profile/", response_model=UserOutDto, status_code=200)
 async def get_profile(
     user: Annotated[User, Depends(get_current_active_user)],
-) -> UserOutSchema:
+) -> UserOutDto:
     """Представление для получения профиля пользователя.
 
     Получаем данные зарегистрированного, аутентифицированного
     и активного пользователя.
     """
-    return UserOutSchema.model_validate(user)
+    return UserOutDto.model_validate(user)
